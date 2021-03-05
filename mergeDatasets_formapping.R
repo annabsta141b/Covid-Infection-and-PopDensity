42a51e07d185c97da72cfd1eda4f43a2bec00e4b
Mary LaPorte <mflaporte@ucdavis.edu>
2021-03-04 17:38:49 -0800

    manual edit fix
    for mapping data

--- mergeDatasets_formapping.R
+++ mergeDatasets_formapping.R
@@ -8,6 +8,11 @@ data(fips_codes)
 #gives us the lat and long
 counties <- read.table('/Users/maryfrancis/Downloads/2020_Gaz_counties_national.txt', sep = "\t", header = TRUE)
 counties_ca <- counties[which(counties$USPS == "CA"),]
+write.csv(counties_ca, '/Users/maryfrancis/Documents/GitHub/Project-3/Data/counties_ca.csv')
+#data cleaning manual (add spaces in county names)
+counties_ca <- read.csv('/Users/maryfrancis/Documents/GitHub/Project-3/Data/counties_ca.csv', header = TRUE)
+
+
 
 #gives us the GEOID (to access lat and long)
 counties_geoid <- counties("California")
